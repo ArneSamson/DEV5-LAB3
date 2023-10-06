3636651b6456a8a5b6e7452de674a60a0b58106f
ArneSamson <arnesamson007@gmail.com>
2023-10-06 12:47:31 +0200

    style: add and remove whitespaces

--- src/App.js
+++ src/App.js
@@ -52,19 +52,18 @@ class App{
         var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
         var dLon = this.deg2rad(lon2-lon1); 
         var a = 
-          Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
-          Math.sin(dLon/2) * Math.sin(dLon/2)
-          ; 
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+            ; 
         var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
         var d = R * c; // Distance in km
         return d;
-      }
+    }
       
     deg2rad(deg) {
         return deg * (Math.PI/180)
-      }
-
+    }
 
     getOccupation(){
 
@@ -92,6 +91,7 @@ class App{
         errorLocation(error) {
             console.log(error);
         }
+        
 }
 
 let app = new App();
